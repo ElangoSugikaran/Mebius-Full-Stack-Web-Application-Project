fee4e8aebe0a181cb45a080006938d4de9f08da5
sugikaran <133148272+sugikaran@users.noreply.github.com>
2025-08-22 12:04:00 +0530

    fix: simplify CORS configuration to allow requests from the frontend URL

--- fed-2-back-end/src/index.ts
+++ fed-2-back-end/src/index.ts
@@ -27,25 +27,7 @@ const app = express();
 app.use(express.json());
 // Use Clerk middleware for authentication
 app.use(clerkMiddleware());
-// app.use(cors({origin: process.env.FRONTEND_URL, credentials: true})); // Enable CORS to allow cross-origin requests
-
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  "http://localhost:5173"
-];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
-
-
+app.use(cors({origin: process.env.FRONTEND_URL, credentials: true})); // Enable CORS to allow cross-origin request
 
 // Webhook endpoint must be raw body
 app.post(
